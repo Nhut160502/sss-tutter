d19bfcceea501cda5edc83ea48a329a570deed38
Nhut160502 <nhut160502@gmail.com>
2023-09-28 09:33:58 +0700

    database

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import 'dotenv/config'
+import cors from 'cors'
+import { connectDatabase } from "./src/configs/database.js";
 const app = express();
-
+app.use(express.json())
+app.use(cors());
 app.use(express.static("./public"));
-
+connectDatabase();
 app.listen(8080, () => {
-  console.log("Server running on port 8080");
+  console.log(`Server running on http://${process.env.HOST}:${process.env.PORT}`);
 });
---
+++ Backend/src/configs/mongoose.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import slug from "mongoose-slug-generator";
+import slugUpdate from "mongoose-slug-updater";
+import mongooseDelete from "mongoose-delete";
+export const configMongoose = (Model) => {
+    mongoose.plugin(slug);
+    mongoose.plugin(slugUpdate);
+    Model.plugin(mongooseDelete, {
+    deletedAt: true,
+    overrideMethods: true,
+    });
+}
